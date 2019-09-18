f33baabcd9253bfa1c447d2a1e06aba036238f25
AkmalKurbanov <250296a@gmail.com>
2019-09-18 20:55:20 +0600

    cookie

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,34 @@
+(function ($) {
+  $(window).on('load', function () {
+    $('.modalScroll-js').mCustomScrollbar({
+      scrollButtons: {
+        enable: true
+      }
+    });
+  });
+})(jQuery);
 
-    (function ($) {
-        $(window).on('load', function () {
-            $('#moadalScroll-js').mCustomScrollbar({
-                scrollButtons:{
-                    enable:true
-                }
-            });
-        });
-    })(jQuery);
+
+
+$('.agree-js').on('click', function () {
+  $('#offer').hide();
+});
+$('.agree-js2').on('click', function () {
+  $('#confidentiality').hide();
+});
+$('.agree-js3').on('click', function () {
+  $('#agree').hide();
+  Cookies.set('modal', 'show', {expires: 7});
+  $('body').removeClass('bodyScroll');
+});
+
+if (!Cookies.get('modal')) {
+  $('.modal-agree').show();
+  $('body').addClass('bodyScroll');
+}
+
+
+
+
+
 
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,12 +1,1 @@
-"use strict";
-
-(function ($) {
-  $(window).on('load', function () {
-    $('#moadalScroll-js').mCustomScrollbar({
-      scrollButtons: {
-        enable: true
-      }
-    });
-  });
-})(jQuery);
-//# sourceMappingURL=main.js.map
+"use strict";!function(o){o(window).on("load",function(){o(".modalScroll-js").mCustomScrollbar({scrollButtons:{enable:!0}})})}(jQuery),$(".agree-js").on("click",function(){$("#offer").hide()}),$(".agree-js2").on("click",function(){$("#confidentiality").hide()}),$(".agree-js3").on("click",function(){$("#agree").hide(),Cookies.set("modal","show",{expires:7}),$("body").removeClass("bodyScroll")}),Cookies.get("modal")||($(".modal-agree").show(),$("body").addClass("bodyScroll"));
